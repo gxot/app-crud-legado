36b48ae6b357006241b96d4759ce23037d180ac0
gxot <gustschott@gmail.com>
2025-09-14 16:16:52 -0300

    Criação da SegundaActivity

---
+++ app/src/main/java/br/pucpr/app/ui/SegundaActivity.java
@@ -0,0 +1,21 @@
+package br.pucpr.app.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import br.pucpr.app.R;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+        setTitle("SegundaActivity");
+    }
+}
